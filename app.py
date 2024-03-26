2e5a38f5a88180fa3d9b52dfcdb0f5025cfa29b3
OlegBW <olegkorostilov58@gmail.com>
2024-03-26 11:23:44 +0200

    init: Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from flask_smorest import Api
+from flask_migrate import Migrate
+
+from db import db
+
+
+def create_app(db_url=None):
+    app = Flask(__name__)
+    load_dotenv()
+
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Forecast REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/docs"
+    app.config[
+        "OPENAPI_SWAGGER_UI_URL"
+    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
+        "DATABASE_URL", "sqlite:///data.db"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    api = Api(app)  # noqa: F841
+    db.init_app(app)
+    migrate = Migrate(app, db)  # noqa: F841
+    return app
---
+++ justfile
@@ -0,0 +1,17 @@
+default:
+    just --list
+
+lint flag="" path=".":
+    @ruff check {{flag}} {{path}}
+
+format flag="" path=".":
+    @ruff format {{flag}} {{path}}
+
+build:
+    @sudo docker build -t forecast-api .
+
+dev:
+    @sudo docker run -d -p 5005:5000 -w /app -v "$(pwd):/app" forecast-api
+
+start:
+    @sudo docker run -d -p 5005:5000 forecast-api
